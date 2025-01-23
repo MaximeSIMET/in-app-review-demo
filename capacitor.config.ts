7ac5ab9bf614688fb92993adabee7ed070b56f54
ms-teamcity <msimet@jnesis.com>
2025-01-23 16:48:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'in-app-review-demo',
+  webDir: 'www'
+};
+
+export default config;
