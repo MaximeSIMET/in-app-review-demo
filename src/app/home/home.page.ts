851450ad2c08049230c4bca2b0df0e517b8557bc
ms-teamcity <msimet@jnesis.com>
2025-01-23 16:55:56 +0100

    add app review btn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { InAppReview } from '../../../../in-app-review';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+
+  async triggerAppReview(): Promise<void> {
+    await InAppReview.requestReview().catch((error) => {
+      console.log(error);
+      alert('requestReview error');
+    });
+  }
 }
